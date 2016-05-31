c81e3d3e9f3266ece4a8582ebc69bdaa1db6ba9e
UmerSani <bsef14a501@pucit.edu.pk>
2016-06-01 00:01:11 +0500

    Update M2-client.py

--- M2-client.py
+++ M2-client.py
@@ -2,10 +2,7 @@ import socket
 import sys
 from thread import *
 
-def read(s):
-	while True:
-		data=recv(1024)
-		print(data)
+
 
                # Import socket module
 
@@ -13,13 +10,17 @@ s = socket.socket()
 host = socket.gethostname()
 port = 5188                # Reserve a port for your service.
 s.connect((host, port))
-#name=input("Enter your Name\n")
-s.sendall("sanii")
-cname=input("Enter Your Friend name \n")
+name=raw_input("Enter your Name\n")
+s.sendall(name)
+
+
+cname=raw_input("Enter Your Friend name \n")
 s.sendall(cname)
+
 data=s.recv(4096)
-print ('list  :-',str(data))
-while (1):
+print 'list  :-', str(data)
+
+while 1:
 	data=s.recv(1024)
 	if data=='connected':
 		break
@@ -27,24 +28,18 @@ while (1):
 		data=s.recv(1024)
 		s.sendall(data)
 
+
+def read(s):
+	while True:
+		data=s.recv(1024)
+		print 'abcd'
+		print data
+
 start_new_thread(read,(s,))
 while True:
-	data=input()
+	data=raw_input()
 	s.sendall(data)
 s.close()
                     # Close the socket when done
 
-def fun(s):
-	
-	while True:
-		data=raw_input()
-		if not data:
-			break
-		else:
-			s.sendall(data)
-			data=s.recv(1024)
-			if not data:
-				break
-			else:
-				print data
-	s.close()
+
